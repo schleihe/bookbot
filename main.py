dbbd3bdd2ff8e7f0746991e576a2318ccfe0aaf7
schleihe <heikoeschleicher@proton.me>
2025-06-15 09:46:33 +0200

    finished bookbot

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from stats import get_word_count
-from stats import get_char_count
+from stats import get_char_count 
+from stats import get_sorted_char_list
+import sys
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
-    text=get_book_text("books/frankenstein.txt")
+    args=sys.argv
+    if(len(args)!=2):
+        print("Invallid arguments")
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    bookpath=args[1]
+    print(f"{bookpath}")
+    text=get_book_text(bookpath)
     num_words=get_word_count(text)
     char_set=get_char_count(text)
-    print(f"{num_words} words found in the document")
-    print(f"{char_set}")
+    sorted=get_sorted_char_list(char_set)
+    #print(f"{num_words} words found in the document")
+    #print(f"{char_set}")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {bookpath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for entry in sorted:
+        print(f"{entry["char"]}: {entry["num"]}")
+    print("============= END ===============")
     #print(get_book_text("books/frankenstein.txt"))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+def sort_on(dict):
+    return dict["num"]
+
 def get_word_count(string):
     return len(string.split())
 
@@ -10,3 +13,14 @@ def get_char_count(text: str) -> set[tuple[str, int]]:
             zaehler[buchstabe] = zaehler.get(buchstabe, 0) + 1
             
     return zaehler
+
+def get_sorted_char_list(zaehler)->list[tuple[str, str],tuple[str,int]]:  
+    sorted=[]  
+    for char in zaehler:
+        #char_tupel= ("char",char)
+        #char_count= ("num",zaehler[char])
+        #instert={char_tupel,char_count}
+        instert={"char":char,"num":zaehler[char]}
+        sorted.append(instert)
+    sorted.sort(reverse=True, key=sort_on)
+    return sorted
